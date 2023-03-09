56145306a1f5cf0f654ba02df075236a99480fff
jose <jose.alvarez@consulting-for.edenred.com>
2023-03-09 13:14:52 -0600

    addViews

---
+++ NotasSwift/NotasSwift/NotaCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NotaCollectionViewCell.swift
+//  NotasSwift
+//
+//  Created by Jose on 09/03/23.
+//
+
+import UIKit
+
+class NotaCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NotasSwift/NotasSwift/RegisterNotaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterNotaViewController.swift
+//  NotasSwift
+//
+//  Created by Jose on 09/03/23.
+//
+
+import UIKit
+
+class RegisterNotaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSave(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
